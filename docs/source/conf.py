a9e8d18ac9b6ff9791af0851ce678a142d151da1
Alberto Finora <alberto.finora@unitn.it>
2024-08-27 09:45:17 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CIMeC Wiki'
+copyright = '2024, Alberto'
+author = 'Alberto'
 
 release = '0.1'
 version = '0.1.0'
